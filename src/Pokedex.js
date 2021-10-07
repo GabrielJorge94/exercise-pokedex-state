02484224b884842565b4b6ce110f1d1511e642fe
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-10-07 18:50:55 -0400

    add button and nextPokemon function

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import Button from './Button';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+
+    constructor(){
+        super()
+
+        this.state = {
+        pokemonIndex:0
+        }
+
+        this.nextPokemon = this.nextPokemon.bind(this);
+    }
+
+    
+    nextPokemon() {
+        if(this.state.pokemonIndex === this.props.pokemons.length-1){
+            this.setState((_estadoAnterior, _props)=> ({pokemonIndex:0}))
+        } else{
+            this.setState((estadoAnterior, _props)=> ({pokemonIndex:estadoAnterior.pokemonIndex + 1}))
+    }}
+
     render() {
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                
+                {<Pokemon pokemon={this.props.pokemons[this.state.pokemonIndex]} />}
+
+                <button onClick={this.nextPokemon}>Next Pokemon</button>
             </div>
         );
     }
